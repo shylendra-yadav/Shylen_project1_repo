b36fc5e839c37ef8456fbae116d4f15251130e35
Shylendra Yadav <shylendrayadav.info@gmail.com>
2023-10-23 17:32:55 +0530

    23oct changes

--- src/test/java/testcase/Desktop.java
+++ src/test/java/testcase/Desktop.java
@@ -1,8 +1,16 @@
 package testcase;
+import java.time.Duration;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -99,37 +107,4 @@ public class Desktop extends BaseTest {
             System.out.println("There are no products to list in this category.");
 }
         }
-	    @Test(priority=5)
-	    public void addToCartComponentsMonitors() throws InterruptedException 
-        {
-		
-		driver.findElement(By.linkText("Components")).click();
-		driver.findElement(By.linkText("Monitors (2)")).click();
-		
-		WebElement prodlist = driver.findElement(By.id("content"));
-		if (prodlist.findElements(By.cssSelector(".product-layout")).size() > 0) {
-			
-			driver.findElement(By.xpath("/html/body/div[2]/div/div/div[3]/div[1]/div/div[2]/div[2]/button[1]/span")).click();
-			driver.findElement(By.name("option[218]")).click();
-			driver.findElement(By.name("option[223][]")).click();
-			driver.findElement(By.id("input-option217")).click();
-			driver.findElement(By.xpath("//option[@value=\"4\"]")).click();
-			driver.findElement(By.id("input-option209")).sendKeys("abc");
-			WebElement fileInput= driver.findElement(By.id("button-upload222"));
-			String filepath = "C:\\Users\\Admin\\Downloads";
-			fileInput.sendKeys(filepath);
-			
-			Alert confirmAlert = driver.switchTo().alert();
-			String alertText = confirmAlert.getText();
-			System.out.println("Alert Text: " + alertText);
-			confirmAlert.accept();
-			driver.findElement(By.id("button-cart")).click();
-			String successMessage = driver.findElement(By.cssSelector(".alert")).getText();
-			Assert.assertTrue(successMessage.contains("Success: You have added"));
-			System.out.println("Item added to the shopping cart.");
-		} else {
-			
-			String noProductsMessage = driver.findElement(By.id("content")).getText();
-            Assert.assertTrue(noProductsMessage.contains("There are no products to list in this category."));
-            System.out.println("There are no products to list in this category.");
-	    }}}
+}
---
+++ src/test/java/testcase/LearnNewConcepts.java
@@ -0,0 +1,18 @@
+package testcase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LearnNewConcepts {
+	
+	WebDriver driver = new ChromeDriver();
+	@Test
+	public void new1()  {
+		
+		
+		
+	}
+	
+
+}
